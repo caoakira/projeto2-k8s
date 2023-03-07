c0f79a1409a6deecd8abff9dbe9529920aba0131
claudio <elliot@DESKTOP-KERPEUS>
2023-03-07 11:43:47 -0300

    Melhoria do código

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "Criando as imagem..."
+echo "Criando a imagem..."
 
 docker build -t caoakira/projeto2-k8s:2.0 app/.
 
 echo "Realizando o push da imagem..."
 
-docker push caoakira/projeto2-k8s:2.0
+docker push akira78/projeto2-k8s:2.0
 
-echo "Criando os serviços do cluster..."
+echo "Criando o serviço do cluster..."
 
 kubectl apply -f ./services.yml
 
-echo "Realizando deployments..."
+echo "Realizando os deployments..."
 
-kubectl apply -f .\mysql-deployment.yml --record
-kubectl apply -f .\app-deployment.yml --record
+kubectl apply -f ./mysql-deployment.yml --record
+kubectl apply -f ./app-deployment.yml --record
